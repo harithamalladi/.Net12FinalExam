f4e156c901f679c823b1afe61857c83478443665
Haritha <haritham18@gmail.com>
2020-11-19 19:58:57 -0500

    first commit

---
+++ Net12FinalExam/Net12FinalExam/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Net12FinalExam
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            IPerson[] array = new IPerson[]
+            {
+                new Adult()
+                { FirstName = "Haritha", LastName = "M" },
+                new Child()
+                { FirstName = "Ishaan", LastName = "K" }
+            };
+
+            foreach (IPerson item in array)
+            {
+                Console.WriteLine("First Name: " + item.FirstName + ", Last Name: " + item.LastName);
+            }
+        }
+    }
+}
